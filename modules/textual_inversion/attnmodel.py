2d33abf282a95daa1f4549f41c46540c30c7532b
Hans <sh_wang@pku.edu.cn>
2024-01-13 18:40:50 +0800

    Add CLIPVisionModelWithProjection and FrozenCLIPVisionencoder classes

--- modules/textual_inversion/attnmodel.py
+++ modules/textual_inversion/attnmodel.py
@@ -2,6 +2,9 @@
 import numpy as np
 from torch import nn
 from modules.attention import CrossAttention
+from transformers import CLIPVisionModelWithProjection, AutoProcessor
+from modules import shared, devices
+
 
 class Attentions(nn.Module):
     def __init__(self, dim, n_heads, d_head, dropout=0., context_dim=None, gated_ff=True, checkpoint=True):
@@ -19,3 +22,20 @@ class Attentions(nn.Module):
         x_2 = self.attn2(x_1, x)
         x_3 = self.net(x_2)
         return x_3
+    
+class FrozenCLIPVisionencoder:
+    def __init__(self, version = "openai/clip-vit-large-patch14", device="cuda", max_length=77):
+        self.processor = AutoProcessor.from_pretrained(version)
+        self.encoder = CLIPVisionModelWithProjection.from_pretrained(version).eval().requires_grad_(False).to(device)
+        self.device = device
+        self.max_length = max_length
+        self.encoder.to(device)
+        self.freeze()
+    
+    def freeze(self):
+        self.encoder.eval()
+        for param in self.encoder.parameters():
+            param.requires_grad = False
+            
+    def encode(self, images):
+        self.processor()
